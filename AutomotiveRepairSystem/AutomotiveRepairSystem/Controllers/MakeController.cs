b59d470ef0f1ba915845fa48a82d3a6962c79801
Thato <t.chitja369@gmail.com>
2025-02-26 15:47:32 +0200

    Add Get request on Create Action of Make Controller

--- AutomotiveRepairSystem/AutomotiveRepairSystem/Controllers/MakeController.cs
+++ AutomotiveRepairSystem/AutomotiveRepairSystem/Controllers/MakeController.cs
@@ -28,5 +28,11 @@ namespace AutomotiveRepairSystem.Controllers
 
             return View();
         }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
     }
 }
